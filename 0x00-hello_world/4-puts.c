7380f5a45db730278a2b29cc6336ccb7db88a48b
root <davidkinyanjuikt157@gmail.com>
2023-09-07 07:20:26 -0700

    betty style is a killer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	 // Exiting with the value 0 to indicate successful execution
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
