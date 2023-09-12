fd86d0efa4f90a0c1450cf1208c74446d7c2f58e
root <davidkinyanjuikt157@gmail.com>
2023-09-12 16:48:10 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
+ */
+
+	void print_alphabet_x10(void)
+	{
+		char ch;
+		int i;
+
+
+		i = 0;
+
+
+		while (i < 10)
+		{
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
