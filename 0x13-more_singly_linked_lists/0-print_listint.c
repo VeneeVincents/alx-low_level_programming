64e0348d68759a9a4d17715092650d074434bf1c
VeneeVincents <davidkinyanjuikt157@gmail.com>
2023-10-23 10:50:49 -0700

    This is the singly linked lists project

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - all elements of a linked list are printed
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+ size_t num = 0;
+
+ while (h)
+ {
+ printf("%d\n", h->n);
+ num++;
+ h = h->next;
+ }
+
+ return (num);
+}
