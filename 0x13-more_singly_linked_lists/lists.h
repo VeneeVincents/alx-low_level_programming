d0aea09584259a9045aa96e6d8013f00ea3fcc08
VeneeVincents <davidkinyanjuikt157@gmail.com>
2023-10-24 02:25:03 -0700

    new change to lists file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list quiz
+ * struct listint_s - singly linked second
  * @n: integer
  * @next: points to the next node
  *
@@ -17,7 +17,6 @@ typedef struct listint_s
  int n;
  struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
